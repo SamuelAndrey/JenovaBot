8231d00d773100bf7bc98343fa21c597124c7b66
samuelandrey <samuelandrey2021@gmail.com>
2023-07-03 15:50:14 +0700

    Add Admin

--- src/main/java/com/samuelandrey/jenovabot/TestBroadcast.java
+++
@@ -1,8 +0,0 @@
-package com.samuelandrey.jenovabot;
-
-public class TestBroadcast {
-    public static void main(String[] args) {
-        JenovaBot jenova = new JenovaBot();
-        jenova.sendBroadcastMessage("omagaaaaa heyeheyeheyeheyeh");
-    }
-}
